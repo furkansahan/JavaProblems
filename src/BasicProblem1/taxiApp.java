41b1b0e44f6e30bc15f955947b571b9588ec8123
Furkan Şahan <mustafafurkansahan@gmail.com>
2022-05-23 13:49:08 +0300

    taxiApp

---
+++ src/BasicProblem1/taxiApp.java
@@ -0,0 +1,22 @@
+package BasicProblem1;
+
+import java.util.Scanner;
+
+public class taxiApp {
+    public static void main(String[] args) {
+        int km,minimumPay; double perKm,startPrice,total;
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the KM: ");
+        km= input.nextInt();
+        perKm=2.20;
+        startPrice=10;
+        minimumPay=20;
+        total=(km*perKm)+startPrice;
+        total= (total<20)? 20: total;
+        System.out.println("Total Price: "+total);
+
+
+
+    }
+
+}
